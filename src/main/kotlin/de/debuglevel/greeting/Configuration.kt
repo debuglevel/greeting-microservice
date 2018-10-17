0aa76ec08975cdb4ede47816850d7652f4d14f2f
Marc Kohaupt <debuglevel@gmail.com>
2018-10-17 22:17:24 +0200

    Add Configuration

---
+++ src/main/kotlin/de/debuglevel/greeting/Configuration.kt
@@ -0,0 +1,32 @@
+package de.debuglevel.greeting
+
+import com.natpryce.konfig.Configuration
+import com.natpryce.konfig.ConfigurationProperties
+import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
+import com.natpryce.konfig.EnvironmentVariables
+import com.natpryce.konfig.overriding
+import java.io.File
+
+object Configuration {
+    val configuration: Configuration
+
+    init {
+        var config: Configuration = systemProperties()
+
+        config = config overriding
+                EnvironmentVariables()
+
+        config = config overriding
+                ConfigurationProperties.fromOptionalFile(File("configuration.properties"))
+
+        val defaultsPropertiesFilename = "defaults.properties"
+        if (ClassLoader.getSystemClassLoader().getResource(defaultsPropertiesFilename) != null) {
+            config = config overriding
+                    ConfigurationProperties.fromResource(defaultsPropertiesFilename)
+        }
+
+        configuration = config
+    }
+
+    //val mongodbUrl = configuration.getOrNull(Key("mongodb.url", stringType)) ?: "localhost:27017"
+}
