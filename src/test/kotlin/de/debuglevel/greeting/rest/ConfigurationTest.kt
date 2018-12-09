a40a090fc767cd54156cfb702b7a98e8e726e9ed
Marc Kohaupt <debuglevel@gmail.com>
2018-12-09 22:19:46 +0100

    Test Configuration

---
+++ src/test/kotlin/de/debuglevel/greeting/rest/ConfigurationTest.kt
@@ -0,0 +1,22 @@
+package de.debuglevel.greeting.rest
+
+import com.natpryce.konfig.Key
+import com.natpryce.konfig.intType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ConfigurationTest {
+
+    @Test
+    fun `port is 80`() {
+        // Arrange
+
+        // Act
+        val port = Configuration.configuration[Key("port", intType)]
+
+        // Assert
+        assertThat(port).isEqualTo(80)
+    }
+}
