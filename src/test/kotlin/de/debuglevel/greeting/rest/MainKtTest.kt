86a087fb6c8c967aaba65c15109c6945ac04d8ea
Marc Kohaupt <debuglevel@gmail.com>
2019-02-26 18:11:13 +0100

    Use / path for testing if server is reachable

--- src/test/kotlin/de/debuglevel/greeting/rest/MainKtTest.kt
+++ src/test/kotlin/de/debuglevel/greeting/rest/MainKtTest.kt
@@ -17,7 +17,7 @@ class MainKtTest {
         Spark.awaitInitialization()
 
         // Act
-        val response = ApiTestUtils.request("GET", "/greetings/test", null)
+        val response = ApiTestUtils.request("GET", "/", null)
 
         // Assert
         // HTTP Codes begin from "100". So something from 100 and above was probably a response to a HTTP request
