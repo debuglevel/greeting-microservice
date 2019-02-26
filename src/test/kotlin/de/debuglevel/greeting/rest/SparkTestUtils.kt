c606f88faea844c931d5daf9621adb2a5369bd01
Marc Kohaupt <debuglevel@gmail.com>
2019-02-26 19:09:55 +0100

    Check internals of Spark to check if it is already shut down.
    The former way still had some issues with race conditions.

--- src/test/kotlin/de/debuglevel/greeting/rest/SparkTestUtils.kt
+++ src/test/kotlin/de/debuglevel/greeting/rest/SparkTestUtils.kt
@@ -2,20 +2,51 @@ package de.debuglevel.greeting.rest
 
 import com.natpryce.konfig.Key
 import com.natpryce.konfig.intType
+import spark.Service
 import spark.Spark
 import java.io.IOException
 import java.net.ServerSocket
 
 object SparkTestUtils {
+    /**
+     * Shutdown spark server and wait until Spark is finally shut down.
+     *
+     * This is actually a rather nasty workaround to close Spark after test and ensure that it's down when we exit this method (the actual stopping is done in a separate Thread; so we have no notification about that).
+     * But even this does not necessarily catch all race conditions.
+     */
     fun awaitShutdown() {
-        // HACK: nasty workaround to close Spark after test and ensure that it's down when we exit the method (stopping is done in a Thread).
-        // But even this does not necessarily catch all race conditions
         Spark.stop()
+
         while (isLocalPortInUse(Configuration.configuration[Key("port", intType)])) {
+            Thread.sleep(100)
+        }
+
+        while (isSparkInitialized()) {
             Thread.sleep(100)
         }
     }
 
+    /**
+     * Access the internals of Spark to check if the "initialized" flag is already set to false.
+     */
+    private fun isSparkInitialized(): Boolean {
+
+        val sparkClass = Spark::class.java
+        val getInstanceMethod = sparkClass.getDeclaredMethod("getInstance")
+        getInstanceMethod.isAccessible = true
+        val service = getInstanceMethod.invoke(null) as Service
+
+        val serviceClass = service::class.java
+        val initializedField = serviceClass.getDeclaredField("initialized")
+        initializedField.isAccessible = true
+        val initialized = initializedField.getBoolean(service)
+
+        return initialized
+    }
+
+    /**
+     * Check if the Spark port could again be opened; if not, it is still in use by Spark.
+     */
     private fun isLocalPortInUse(port: Int): Boolean {
         return try {
             // ServerSocket try to open a LOCAL port
