bc798713e152ab39a0eb86caa127d4c0d0f698fd
Marc Kohaupt <debuglevel@gmail.com>
2019-08-17 21:36:57 +0200

    Use logback as logging backend; keep kotlin-logging as frontend

--- src/main/kotlin/de/debuglevel/greeter/Application.kt
+++ src/main/kotlin/de/debuglevel/greeter/Application.kt
@@ -2,6 +2,7 @@ package de.debuglevel.greeter
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.runtime.Micronaut
+import mu.KotlinLogging
 
 /**
  * Application entry point.
@@ -11,18 +12,18 @@ import io.micronaut.runtime.Micronaut
  * @param args parameters from the command line call
  */
 object Application {
+    private val logger = KotlinLogging.logger {}
+
     lateinit var applicationContext: ApplicationContext
 
     @JvmStatic
     fun main(args: Array<String>) {
+        logger.info { "Starting up..." }
         applicationContext = Micronaut.run(Application.javaClass)
 
-        // TODO: log the accessed URLs
-        // TODO: log application startup
         // TODO: how to do API versioning? (or do it at all?)
         // TODO: do configuration with some Micronaut provided stuff instead of natpryce.konfig?
         // TODO: prettier JSON output?
-        // TODO: switch to other log backend?
         // TODO: create a README.md which is usable as a template
         // TODO: retain parameter names via compileJava.options.compilerArgs += '-parameters' or/and? https://docs.micronaut.io/latest/guide/index.html#kotlinretainparamnames
 
