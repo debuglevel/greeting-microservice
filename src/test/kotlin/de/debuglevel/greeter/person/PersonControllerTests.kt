9945714c1fe27ca96876587f8f4679b1c1dbb014
Marc Kohaupt <debuglevel@gmail.com>
2019-08-21 18:59:51 +0200

    Test PersonController

--- src/test/kotlin/de/debuglevel/greeter/person/PersonControllerTests.kt
+++ src/test/kotlin/de/debuglevel/greeter/person/PersonControllerTests.kt
@@ -1,9 +1,13 @@
 package de.debuglevel.greeter.person
 
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import javax.inject.Inject
 
@@ -17,34 +21,41 @@ class PersonControllerTests {
     @field:Client("/persons")
     lateinit var httpClient: HttpClient
 
-//    @Test
-//    fun `save person`() {
-//        // Arrange
-//        val uri = UriBuilder.of("/{name}")
-//            .expand(mutableMapOf("name" to person.name))
-//            .toString()
-//
-//        // Act
-//        // TODO: ... POST
-//        val savedPerson = httpClient.toBlocking().retrieve(uri)
-//
-//        // Assert
-//        Assertions.assertThat(savedPerson).contains(person.name)
-//    }
-//
-//    @Test
-//    fun `retrieve person`() {
-//        // Arrange
-//        // TODO: ... POST person
-//        val uri = UriBuilder.of("/{id}")
-//            .expand(mutableMapOf("name" to savedPerson.id))
-//            .toString()
-//
-//        // Act
-//        val retrievedPerson = httpClient.toBlocking().retrieve(uri)
-//
-//        // Assert
-//        Assertions.assertThat(retrievedPerson).contains(person.id)
-//        Assertions.assertThat(retrievedPerson).contains(person.name)
-//    }
+    @Test
+    fun `save person`() {
+        // Arrange
+        val person = Person(0, "Mozart")
+
+        // Act
+        val uri = UriBuilder.of("/{name}")
+            .expand(mutableMapOf("name" to person.name))
+            .toString()
+        val savedPerson = httpClient.toBlocking()
+            .retrieve(HttpRequest.POST(uri, ""), PersonDTO::class.java)
+
+        // Assert
+        Assertions.assertThat(savedPerson.name).isEqualTo(person.name)
+    }
+
+    @Test
+    fun `retrieve person`() {
+        // Arrange
+        val person = Person(0, "Mozart")
+        val saveUri = UriBuilder.of("/{name}")
+            .expand(mutableMapOf("name" to person.name))
+            .toString()
+        val savedPerson = httpClient.toBlocking()
+            .retrieve(HttpRequest.POST(saveUri, ""), PersonDTO::class.java)
+
+        // Act
+        val retrieveUri = UriBuilder.of("/{id}")
+            .expand(mutableMapOf("id" to savedPerson.id))
+            .toString()
+        val retrievedPerson = httpClient.toBlocking()
+            .retrieve(retrieveUri, PersonDTO::class.java)
+
+        // Assert
+        Assertions.assertThat(retrievedPerson.id).isEqualTo(savedPerson.id)
+        Assertions.assertThat(retrievedPerson.name).isEqualTo(savedPerson.name)
+    }
 }
---
+++ src/test/kotlin/de/debuglevel/greeter/person/PersonDTO.kt
@@ -0,0 +1,7 @@
+package de.debuglevel.greeter.person
+
+data class PersonDTO(
+    val id: Long,
+    val name: String
+) {
+}
