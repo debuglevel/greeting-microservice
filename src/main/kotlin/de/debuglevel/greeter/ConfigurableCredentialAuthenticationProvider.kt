1bb969e354c5c3e48d8bf94885559b7011ce7b60
Marc Kohaupt <debuglevel@gmail.com>
2020-06-21 23:53:23 +0200

    Migrate Micronaut Security custom ConfigurableCredentialAuthenticationProvider

--- src/main/kotlin/de/debuglevel/greeter/ConfigurableCredentialAuthenticationProvider.kt
+++ src/main/kotlin/de/debuglevel/greeter/ConfigurableCredentialAuthenticationProvider.kt
@@ -1,8 +1,11 @@
 package de.debuglevel.greeter
 
 import io.micronaut.context.annotation.Property
+import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.*
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.FlowableEmitter
 import org.reactivestreams.Publisher
 import java.util.*
 import javax.inject.Singleton
@@ -15,13 +18,19 @@ class ConfigurableCredentialAuthenticationProvider(
     @Property(name = "app.security.configurable-credential-authentication.username") val username: String,
     @Property(name = "app.security.configurable-credential-authentication.password") val password: String
 ) : AuthenticationProvider {
-    override fun authenticate(authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
-        if (authenticationRequest.identity == username &&
-            authenticationRequest.secret == password
-        ) {
-            return Flowable.just(UserDetails(username, ArrayList()))
-        } else {
-            return Flowable.just(AuthenticationFailed())
-        }
+    override fun authenticate(
+        httpRequest: HttpRequest<*>?,
+        authenticationRequest: AuthenticationRequest<*, *>
+    ): Publisher<AuthenticationResponse> {
+        return Flowable.create({ emitter: FlowableEmitter<AuthenticationResponse> ->
+            if (authenticationRequest.identity == username &&
+                authenticationRequest.secret == password
+            ) {
+                emitter.onNext(UserDetails(authenticationRequest.identity as String, ArrayList()))
+            } else {
+                emitter.onError(AuthenticationException(AuthenticationFailed()))
+            }
+            emitter.onComplete()
+        }, BackpressureStrategy.ERROR)
     }
 }
