f2582d2bba581f5ca5cbba475bef215a0726a6a1
Marc Kohaupt <debuglevel@gmail.com>
2021-06-04 21:07:03 +0200

    Test gRPC endpoint

---
+++ src/test/kotlin/de/debuglevel/greeter/greeting/GreetingEndpointTests.kt
@@ -0,0 +1,55 @@
+package de.debuglevel.greeter.greeting
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assumptions
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import javax.inject.Inject
+
+@MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GreetingEndpointTests {
+    @Inject
+    lateinit var greetingBlockingStub: GreetingGrpc.GreetingBlockingStub
+
+    @ParameterizedTest
+    @MethodSource("validNameAndLanguageProvider")
+    fun `greet valid names in various languages`(testData: TestDataProvider.NameTestData) {
+        // Assume
+        // the gRPC method wants a non-null language; therefore skip a test if language is null
+        Assumptions.assumeThat(testData.language).isNotNull
+
+        // Arrange
+        val greetRequest = GreetRequest.newBuilder()
+            .setName(testData.name)
+            .setLocale(testData.language)
+            .build()
+
+        // Act
+        val greetReply = greetingBlockingStub.greet(greetRequest)
+
+        // Assert
+        Assertions.assertThat(greetReply.message).contains(testData.expected)
+    }
+
+    fun validNameAndLanguageProvider() = TestDataProvider.validNameAndLanguageProvider()
+
+    // do not test "invalid" names (i.e. "" and " ") as their HTTP call would translate to "/greetings/" which then returns the getList() stuff
+//    @ParameterizedTest
+//    @MethodSource("invalidNameProvider")
+//    fun `greet invalid names`(testData: TestDataProvider.NameTestData) {
+//        // Arrange
+//
+//        // Act
+//        val greeting = httpClient.toBlocking().retrieve("/${testData.name}?language=${testData.language}")
+//
+//        // Assert
+//        Assertions.assertThat(greeting).contains("500")
+//    }
+//
+//    fun invalidNameProvider(): Stream<TestDataProvider.NameTestData> {
+//        return TestDataProvider.invalidNameProvider()
+//    }
+}
